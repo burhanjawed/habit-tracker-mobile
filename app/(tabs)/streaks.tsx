fb5f3995fd8fc0f3f1391015c7fe1fb40e6cd4c9
Burhan Jawed <71589999+tibsxi@users.noreply.github.com>
2025-08-14 00:24:17 -0400

    Added completed habit logic

---
+++ app/(tabs)/streaks.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const StreaksScreen = () => {
+  return (
+    <View>
+      <Text>Hello this is the streaks page</Text>
+    </View>
+  );
+};
+
+export default StreaksScreen;
